48603ee36c80920d913ca780014eca1213671e9c
Elon Flegenheimer <elonf@hotmail.com>
2015-02-27 21:27:31 -0700

    updated weather

--- bin/weather.rb
+++ bin/weather.rb
@@ -8,9 +8,9 @@ begin
   url = 'http://www.accuweather.com/en/us/scottsdale-az/85255/current-weather/36679_pc'
   doc = Nokogiri::HTML(open(url))
 
-  current = doc.css('div.forecast div.info span.temp')[0].content
-  high = doc.css('div.bg.bg-su div.info strong.temp')[0].content
-  sunset = doc.css('div#feature-sun.feature.feature-first p.time-period span.finish')[0].content
+  current = doc.css('div.forecast div.info span.temp')[0].content rescue '?'
+  high = doc.css('div.bg.bg-su div.info strong.temp')[0].content rescue '?'
+  sunset = doc.css('div#feature-sun.feature.feature-first p.time-period span.finish')[0].content rescue ''
   sunset = sunset.gsub(' ', '')
 
   # apparently dzen doesn't like utf-8...
